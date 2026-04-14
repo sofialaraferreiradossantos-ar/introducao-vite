0005847c390f1eb6085476fbded4815ff0d8a972
sofialaraferreiradossantos-ar <sofialaraferreiradossantos@gmail.com>
2026-04-14 11:41:49 -0300

    14/04/2026
    aula 14/04

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -11,7 +11,7 @@ function Home() {
         <h2>Exemplos</h2>
         <Link to="/exemplo/1">Exemplo 1 - Componente básico</Link>
         <Link to="/exemplo/2">Exemplo 2 - Uso de module CSS</Link>
-        <Link to="/exemplo/3">Exemplo 3 - Uso de module CSS</Link>
+        <Link to="/exemplo/3">Exemplo 3 - Contador</Link>
       </div>
 
       <div className={styles.containerCards}>
